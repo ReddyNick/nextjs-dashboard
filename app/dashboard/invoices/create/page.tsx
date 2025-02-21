493659b5fb9eaf01117468f8d1f9562a0959daab
Nikita Chuzhakin <mango.syrup@yandex.ru>
2025-02-21 15:50:53 +0300

    final changes

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,25 @@
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+import { db } from '@vercel/postgres';
+
+export default async function Page() {
+  const client = await db.connect();
+  const customers = await fetchCustomers(client);
+ 
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
